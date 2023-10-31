3a58e7e0e0c56841b31a2ec6d4fa131c241687be
Kolade <akoladesalako5@gmail.com>
2023-10-31 21:31:00 +0100

    First version of my-portfolio

---
+++ src/data.js
@@ -0,0 +1,64 @@
+export const projects = [
+  {
+    title: "Wiki",
+    subtitle: "A Wikipedia-like Online Encyclopedia",
+    description:
+      "I created a Wikipedia-style online encyclopedia from scratch. Users can explore, edit, and search for articles on a wide range of topics. This project showcases my web development skills using Python and Django. It's an interactive, user-friendly online encyclopedia that I designed and built.",
+    image: "./project_2.png",
+    link: "https://github.com/Kolade-dotcom/wiki",
+  },
+  {
+    title: "Auctions",
+    subtitle: "An E-commerce Auction Platform",
+    description:
+      "I developed a feature-rich e-commerce platform with auction capabilities. Users can register, list items for sale, and engage in auction-style bidding. This project demonstrates my proficiency in web development, including user authentication and real-time updates using Django, Python, and JavaScript. It's a comprehensive solution for online buying and selling.",
+    image: "./listings.png",
+    link: "https://github.com/Kolade-dotcom/auctions",
+  },
+  {
+    title: "Finance",
+    subtitle: "A Web-Based Financial Portfolio Management App",
+    description:
+      "I developed a web-based finance application that allows users to manage their financial portfolios. With this project, I gained hands-on experience in web development, implementing features like user registration, stock trading, and portfolio management. Using Python, Flask, and SQLite, I created a secure and user-friendly platform for tracking investments, buying and selling stocks, and monitoring financial performance. This project not only demonstrates my technical skills but also showcases my ability to create practical solutions for real-world problems in the financial domain.",
+    image: "./project_3.png",
+    link: "https://github.com/code50/109832012/tree/main/finance",
+  },
+  {
+    title: "Birthday Reminder",
+    subtitle: "A Simple Web App to Remember Special Occasions",
+    description:
+      " In the Birthday Reminder project, I designed and built a user-friendly web application to help users remember and celebrate special occasions. This project provided a hands-on opportunity to explore web development, using HTML, CSS, JavaScript, and Flask. Users can input and manage important dates, receive timely reminders, and even personalize their celebration messages. The Birthday Reminder project showcases my ability to create practical and engaging web solutions for everyday life, enhancing the user experience and making it easier to celebrate life's milestones.",
+    image: "./project_4.png",
+    link: "https://github.com/code50/109832012/tree/main/pset/birthdays",
+  },
+];
+
+// export const testimonials = [
+//   {
+//     quote:
+//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+//     image: "https://randomuser.me/api/portraits/men/1.jpg",
+//     name: "Jesse Hicks",
+//     company: "Zoozle",
+//   },
+//   {
+//     quote:
+//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+//     image: "https://randomuser.me/api/portraits/men/94.jpg",
+//     name: "Ruben Alvarez",
+//     company: "Dooble.io",
+//   },
+// ];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Python",
+  "Node",
+  "Django",
+  "Figma",
+  "Git/Github",
+  "Bootstrap",
+  "HTML",
+  "CSS"
+];
